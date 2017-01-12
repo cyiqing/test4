f7ebd4cf01a1a3ec8a0dab336be48ed1d7b73783
cyiqing <Qing@qingdeMacBook-Pro.local>
2017-01-12 17:34:28 +0100

    1st_commit
    test1

---
+++ aaa.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+import tushare as ts
+
+df = ts.get_tick_data('600848', date='2016-07-22')
+engine = create_engine('mysql://user:123456@127.0.0.1/Qing?charset=utf8')
+
+#存入数据库
+df.to_sql('tick_data',engine)
+#test
